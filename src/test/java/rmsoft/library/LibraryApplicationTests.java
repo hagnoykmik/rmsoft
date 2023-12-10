b2fa2cd2e259876818d8b9a3cf2e5a75a44c2f50
hagnoykmik <ymj02148@naver.com>
2023-12-10 20:41:15 +0900

    initial configuration

---
+++ src/test/java/rmsoft/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package rmsoft.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
