1f0a477fe7f9bab1ae0429a3a126452f04c25f94
hagnoykmik <ymj02148@naver.com>
2024-01-07 19:51:36 +0900

    FIX: 중복 이메일 검증 로직 추가

--- src/main/java/rmsoft/library/common/exception/ErrorCode.java
+++ src/main/java/rmsoft/library/common/exception/ErrorCode.java
@@ -15,11 +15,12 @@ public enum ErrorCode {
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
     NOT_FOUND_BOOK(HttpStatus.NOT_FOUND, "존재하지 않는 도서입니다."),
     NOT_FOUND_LOAN(HttpStatus.NOT_FOUND, "대출이력이 존재하지 않습니다."),
+    NOT_FOUND_BORROWED_BOOK(HttpStatus.NOT_FOUND, "대출 목록에 해당 도서가 없습니다."),
 
     //409 CONFLICT
     ALREADY_BORROWED_BOOK(HttpStatus.CONFLICT, "이미 대출중인 도서입니다."),
-    ALREADY_RETURNED_BOOK(HttpStatus.CONFLICT, "이미 반납처리된 도서입니다.");
-
+    ALREADY_RETURNED_BOOK(HttpStatus.CONFLICT, "이미 반납처리된 도서입니다."),
+    ALREADY_EXISTED_USER(HttpStatus.CONFLICT, "이미 가입된 유저입니다.");
 
     private final HttpStatus status;
     private final String message;
